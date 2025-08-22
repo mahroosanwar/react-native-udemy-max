13b5ba62fd881b9b3edde6188b887d9ccc3fdaed
Mahroos Anwar <mahroos.anwar0901@gmail.com>
2025-08-22 23:43:06 +0530

    Enhance StartGameScreen: add input handling for number entry, validation logic, and connect buttons to handlers; update PrimaryButton to accept onPress prop.

--- GuessMyNumberGame/app/index.tsx
+++ GuessMyNumberGame/app/index.tsx
@@ -1,11 +1,20 @@
 import { LinearGradient } from "expo-linear-gradient";
-import { StyleSheet } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import StartGameScreen from "../screens/StartGameScreen";
 
 export default function Index() {
   return (
     <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
-      <StartGameScreen />;
+      <ImageBackground
+        source={{
+          uri: "https://images.unsplash.com/photo-1570303345338-e1f0eddf4946?q=80&w=1071&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        }}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />;
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -13,5 +22,8 @@ export default function Index() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
